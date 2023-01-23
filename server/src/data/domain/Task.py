9b69f92d26d11f936ef4470c75d874f044f59ade
Danel Arias <71316377+DanelArias-Dreyton257@users.noreply.github.com>
2023-01-23 11:20:37 +0100

    Added types to the constructors

--- server/src/data/domain/Task.py
+++ server/src/data/domain/Task.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
+
 class Task:
 
-    def __init__(self, name, deadline, expected_duration, importance):
+    def __init__(self, name: str, deadline: datetime, expected_duration_h: int, importance: int):
         self.name = name
         self.deadline = deadline
-        self.expected_duration = expected_duration
+        self.expected_duration_h = expected_duration_h
         self.importance = importance
 
     def __repr__(self):
-        return f"Task(name={self.name}, deadline={self.deadline}, expected_duration={self.expected_duration}, importance={self.importance})"
+        return f"Task(name={self.name}, deadline={self.deadline}, expected_duration_h={self.expected_duration_h}, importance={self.importance})"
+
+    def get_time_until_deadline(self):
+        return (self.deadline - datetime.now()).total_seconds()
--- server/src/data/domain/User.py
+++ server/src/data/domain/User.py
@@ -1,6 +1,6 @@
 class User:
 
-    def __init__(self, username, password, email):
+    def __init__(self, username: str, password: str, email: str):
         self.username = username
         self.password = password
         self.email = email
